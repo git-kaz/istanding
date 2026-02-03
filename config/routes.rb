a4c18e54d56114f7d7c2689d934ab834dba4209e
git-kaz <okw.kzk@gmail.com>
2026-02-03 21:06:23 +0900

    Add guest login機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :exercises, only: [ :index, :show ]
 
   devise_for :users
+
+  post "guest_login", to: "users/guest_sessions#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
