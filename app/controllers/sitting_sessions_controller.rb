d21407db2a594e8bee416a94d7184c9a64a32b64
git-kaz <okw.kzk@gmail.com>
2026-02-06 07:22:35 +0900

    Add durationカラムの追加、コントローラーの記述

--- app/controllers/sitting_sessions_controller.rb
+++ app/controllers/sitting_sessions_controller.rb
@@ -1,7 +1,37 @@
 class SittingSessionsController < ApplicationController
+
+  before_action :authenticate_user!
+
   def new
   end
 
+  def create
+
+    @sitting_session = current_user.sitting_sessions.build(session_params)
+
+    if @sitting_session.save
+      #座位時間が保存されたら運動をランダムに提案
+      @recommended_exercise = Exercise.all.sample
+
+      render json: {
+        status: :created,
+        exercise: @recommended_exercise
+      }
+    else
+      render json: {
+        status: :unprocessable_entity,
+      }, status: :unprocessable_entity
+    end
+  end
+
+  end
+
   def show
+  end
+
+  private
+
+  def session_params
+    params.require(:sitting_session).permit(:duration, :start_at)
   end
 end
--- db/queue_schema.rb
+++ db/queue_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_02_123308) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_05_214818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_02_123308) do
   create_table "sitting_sessions", force: :cascade do |t|
     t.string "auth"
     t.datetime "created_at", null: false
+    t.integer "duration"
     t.string "endpoint"
     t.boolean "notified"
     t.datetime "notify_at"
