6af463925134ce6b85c11743699587ccac0b0977
git-kaz <okw.kzk@gmail.com>
2026-02-06 22:13:45 +0900

    Add timer controllerの実装

---
+++ app/javascript/controllers/timer_controller.js
@@ -0,0 +1,42 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="timer"
+export default class extends Controller {
+    static targets = ["display"]
+    connect() {
+        //開始時間
+        this.startTime = new Date()
+        this.startTimer()
+    }
+    
+    startTimer() {
+        this.timer = setInterval(() => {
+
+        }, 1000)
+    }
+    //運動の選択orスキップ後の処理
+    finish(event) {
+        const endTime = new Date()
+        const duration = Math.floor((endTime - this.startTime) / 1000)
+
+        //RailsのAPIにデータを飛ばす
+        fetch("/sitting_sessions", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content
+            },
+            body: JSON.stringify({
+                sitting_session: {
+                    duration: duration,
+                    start_at: this.startTime
+                }
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                //data.exerciseの運動をモーダル表示
+                this.showRecommendationModal(data.exercise)
+            })
+    }
+}
