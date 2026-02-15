0a02614ea4c7afe0b34e195665fac4427b854407
git-kaz <okw.kzk@gmail.com>
2026-02-15 20:32:52 +0900

    Update ログイン時のヘッダー作成

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="dropdown"
+export default class extends Controller {
+    static targets = ["menu"]
+
+    toggle() {
+        this.menuTarget.classList.toggle("hidden")
+    }
+
+    hide(event) {
+        //メニュー外のクリックで閉じる
+        if (!this.element.contains(event.target)) {
+            this.menuTarget.classList.add("hidden")
+        }
+    }
+}
