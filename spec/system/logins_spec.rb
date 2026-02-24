351935f7dd188ae095cd35c7503e22ddc2a80c1d
git-kaz <okw.kzk@gmail.com>
2026-02-25 08:35:22 +0900

    add system spec作成

--- spec/system/logins_spec.rb
+++ spec/system/logins_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "ログイン機能", type: :system do
     visit new_user_session_path
     fill_in "メールアドレス", with: user.email
     fill_in "パスワード", with: user.password
-    click_button "ログインする"
+    click_button "ログイン"
     expect(page).to have_current_path root_path
   end
   
--- spec/system/sitting_sessions_spec.rb
+++ spec/system/sitting_sessions_spec.rb
@@ -1,14 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "SittingSessions", type: :system do
+  let!(:user) { create(:user) }
+  let!(:exercise) { create(:exercise, name: "肩甲骨ストレッチ") }
+
   before do
-    driven_by(:rack_test)
+     driven_by(:rack_test) #JSなしで動かす
+
+    # ログイン済みの状態にする（共通化している場合はそのメソッドを呼ぶ）
+    visit new_user_session_path
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: user.password
+    click_button "ログイン"
   end
 
-  it "トップページにアクセスできること" do
+  it "タイマーを開始し、休憩ボタンから運動を選択できること" do
+   
+
     visit root_path
-    expect(page).to have_content "座りすぎは喫煙と同等に身体に悪影響を及ぼすとされています"
-  end
 
-  
+    # 1. タイマー時間の選択（例: 30分）
+    click_on "30min"
+    click_on "スタート"
+
+    # 2. 画面上に「休憩する」ボタンが出現することを確認
+    expect(page).to have_content "休憩する"
+  end
 end
