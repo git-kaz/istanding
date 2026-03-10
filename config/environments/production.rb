2e04ed660aa9c960a625f0103afe2c100dae5d14
git-kaz <okw.kzk@gmail.com>
2026-03-10 21:54:15 +0900

    update ホストを独自ドメインに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,9 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  config.hosts << "istanding.jp"      # 独自ドメイン 
+  config.hosts << "www.istanding.jp"  # サブドメイン
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
