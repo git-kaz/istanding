5a7f182074d5825fe76421cb2165fb30b3327d6b
git-kaz <okw.kzk@gmail.com>
2026-04-01 06:17:37 +0900

    add activityLogのバリデーション設定とenumの修正

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -3,5 +3,17 @@ class ActivityLog < ApplicationRecord
   belongs_to :exercise
   belongs_to :sitting_session, optional: true
 
+  # 自由運動の場合のバリデーション(categoryがother）
+  with_options if if: :free_exercise_log? do
+    validates :note, presence:  true, length: { maximum: 50 }
+    validates :duration_minutes, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  end
+
   scope :today, -> { where(created_at: Time.zone.now.all_day) }
+
+  private
+
+  def free_exercise_log?
+    exercise&.category == "other"
+  end
 end
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,6 +1,6 @@
 class Exercise < ApplicationRecord
   has_one_attached :image
-  enum :category, { stretch: 0, training: 1, walk: 2 }
+  enum :category, { stretch: 0, training: 1, other: 2 }
 
   validates :name, presence: true
   validates :category, presence: true
