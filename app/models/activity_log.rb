807268043b769981abcd381a98fd28b7ec2e6978
git-kaz <okw.kzk@gmail.com>
2026-04-01 06:47:47 +0900

    add 自由運動の回復ロジックを作成

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -4,7 +4,7 @@ class ActivityLog < ApplicationRecord
   belongs_to :sitting_session, optional: true
 
   # 自由運動の場合のバリデーション(categoryがother）
-  with_options if if: :free_exercise_log? do
+  with_options if: :free_exercise_log? do
     validates :note, presence:  true, length: { maximum: 50 }
     validates :duration_minutes, presence: true, numericality: { only_integer: true, greater_than: 0 }
   end
@@ -12,6 +12,23 @@ class ActivityLog < ApplicationRecord
   scope :today, -> { where(created_at: Time.zone.now.all_day) }
 
   private
+
+  # userの回復ロジックを使う
+  def apply_hp_recovery
+    user.recover(calculate_recovery_amount)
+  end
+
+  def calculate_recovery_amount
+    # 自由運動の際の回復量を設定（コンセプト上まとめて行っても回復量は増えない）
+    if free_exercise_log?
+      if duration_minutes <= 20
+        duration_minutes
+      else
+        # 20分以上まとめて運動しても回復量は比例しない
+        (20 + (duration_minutes - 20) * 0.2).round
+      end
+    end
+  end
 
   def free_exercise_log?
     exercise&.category == "other"
