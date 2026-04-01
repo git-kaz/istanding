b1f1cee6d1df403ff5282d14d3bfbb385fab9234
git-kaz <okw.kzk@gmail.com>
2026-04-01 14:46:11 +0900

    fix endの追加にて修正

--- db/migrate/20260401024910_remove_active_from_exercises.rb
+++ db/migrate/20260401024910_remove_active_from_exercises.rb
@@ -2,6 +2,7 @@ class RemoveActiveFromExercises < ActiveRecord::Migration[8.1]
   def change
     # column_exists? を使って、カラムがある時だけ削除するようにガードを入れる
     if column_exists?(:exercises, :active)
-    remove_column :exercises, :active, :boolean
+      remove_column :exercises, :active, :boolean
+    end
   end
 end
