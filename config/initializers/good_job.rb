2b4feab9b87cf28cb92bdc8181e80a24e9da7907
git-kaz <okw.kzk@gmail.com>
2026-06-25 17:24:22 +0900

    fix HPの定期リセットの設定

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -3,4 +3,12 @@ Rails.application.configure do
   config.good_job.queues = "default"
   config.good_job.max_threads = 2
   config.good_job.poll_interval = 30
+
+  config.good_job.enable_cron = true
+  config.good_job.cron = {
+    "reset_user_hp" => {
+      cron: "0 2 * * *", # 毎日2時に実行
+      class: "ResetUserHpJob"
+    }
+  }
 end
